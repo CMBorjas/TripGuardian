3ba4dd37bf1b3804b4354659d309bd0af0195b02
CMBorjas <118933346+CMBorjas@users.noreply.github.com>
2025-04-16 23:05:00 -0600

    Added caculation to route based on delay, walk time, crowdedness, and safety(mock).
    Added init file for package usage.
    
    aded append path for directory lookup.

---
+++ src/reranker.py
@@ -0,0 +1,16 @@
+# src/reranker.py
+import pandas as pd
+def rerank_routes(df):
+    """
+    Calculate a total route score based on delay, walktime,crowidn and safety.
+    Higher is better.
+    """
+    df["total_score"] = (
+        100
+        - (df["walk_time"] * 1.5
+        + df["delay_minutes"] * 2
+        + df["crowdedness_score"] * 1.2
+        + (100 - df["safety_score"]))
+    )
+    df = df.sort_values("total_score", ascending=False)
+    return df
