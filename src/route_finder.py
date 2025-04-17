fd9f5ed89aff001b5c11c3e4f6a186879015a90e
CMBorjas <118933346+CMBorjas@users.noreply.github.com>
2025-04-16 22:22:44 -0600

    added gitignore to keep certain files out of repository.
    Reranking logic notebook added to verify routes.
    
    route_finder.py added fucntion to find routes between two points.

--- src/route_finder.py
+++ src/route_finder.py
@@ -1,11 +1,15 @@
 # file used to be able to find the route between two points
 
 def find_routes(start, end, route_graph):
-    posible_routes = [] # list of all possible routes
-        for route_id, stops in route_graph.items():
-            if start in stops and end in stops:
-                possible_routes.append({
-                    "route_id": route_id,
-                    ""
-
-                })
+    possible_routes = [] # list of all possible routes
+    # iterate through all routes in the route graph
+    for route_id, stops in route_graph.items():
+        if start in stops and end in stops:
+            possible_routes.append({
+                "route_id": route_id,
+                "origin": start,
+                "destination": end,
+                "stops":stops[stops.index(start):stops.index(end)+1],
+                "bus_time": len(stops[stops.index(start):stops.index(end)+1]) * 3, # simulation
+            })
+    return possible_routes # return all possible routes between two points
